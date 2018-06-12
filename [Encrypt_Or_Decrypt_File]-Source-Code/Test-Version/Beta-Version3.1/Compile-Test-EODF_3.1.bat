ce4fae270719b36fb38cf82fb11bf05562c6b712
Twilight-Dream-Of-Magic <1187791459@qq.com>
2018-06-12 22:05:32 +0800

    Fix all source code, Update in 2018-6-12

--- [Encrypt_Or_Decrypt_File]-Source-Code/Test-Version/Beta-Version3.1/Compile-Test-EODF_3.1.bat
+++ [Encrypt_Or_Decrypt_File]-Source-Code/Test-Version/Beta-Version3.1/Compile-Test-EODF_3.1.bat
@@ -12,10 +12,10 @@ Cls
 Echo "---------- Code Compile Start ----------"
 :: g++.exe -shared -o
 :: -fexec-charset=GB2312
-g++.exe -O2 -c ".\\Main_EODF3-1_By_Twlilght_Yujiang.cpp" -o "..\\..\\..\\Test_Binary\\Beta-Vesion3.1\\CUI_EncryptDecryptFile_TestVersion3-1.binary-objective"
+g++.exe -O2 -c ".\\Main_EODF3-1_By_TDOM.cpp" -o "..\\..\\..\\Test_Binary\\Beta-Vesion3.1\\CUI_EncryptDecryptFile_TestVersion3-1.objective-binary"
 :: g++.exe -finput-charset=UTF-8
-g++.exe -shared "..\\..\\..\\Test_Binary\\Beta-Vesion3.1\\CUI_EncryptDecryptFile_TestVersion3-1.binary-objective" -o "..\\..\\..\\Test_Binary\\Beta-Vesion3.1\\CUI_EncryptDecryptFile_TestVersion3-1.exe"
-g++.exe -static "..\\..\\..\\Test_Binary\\Beta-Vesion3.1\\CUI_EncryptDecryptFile_TestVersion3-1.binary-objective" -o "..\\..\\..\\Test_Binary\\Beta-Vesion3.1\\CUI_EncryptDecryptFile_TestVersion3-1_StaticLink.exe"
+g++.exe -shared "..\\..\\..\\Test_Binary\\Beta-Vesion3.1\\CUI_EncryptDecryptFile_TestVersion3-1.objective-binary" -o "..\\..\\..\\Test_Binary\\Beta-Vesion3.1\\CUI_EncryptDecryptFile_TestVersion3-1.exe"
+g++.exe -static "..\\..\\..\\Test_Binary\\Beta-Vesion3.1\\CUI_EncryptDecryptFile_TestVersion3-1.objective-binary" -o "..\\..\\..\\Test_Binary\\Beta-Vesion3.1\\CUI_EncryptDecryptFile_TestVersion3-1_StaticLink.exe"
 Echo "---------- Code Compile End ----------"
 Pause
 Cls
